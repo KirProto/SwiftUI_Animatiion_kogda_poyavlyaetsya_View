d7b018dbddb17edace33a6f2c50b51bd5d7ff9a0
Kir Proto Q <kir.proto@ya.ru>
2022-02-11 12:22:26 +0500

    Initial Commit

---
+++ Yan_SwiftUI_Animatiion_kogda_poyavlyaetsya_View/Yan_SwiftUI_Animatiion_kogda_poyavlyaetsya_ViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yan_SwiftUI_Animatiion_kogda_poyavlyaetsya_ViewApp.swift
+//  Yan_SwiftUI_Animatiion_kogda_poyavlyaetsya_View
+//
+//  Created by KIR Q on 2022.02.11.
+//
+
+import SwiftUI
+
+@main
+struct Yan_SwiftUI_Animatiion_kogda_poyavlyaetsya_ViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
